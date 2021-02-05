488a2b64e332de810de8c3f7251d218fef6d3f86
Paul Yi <pauljsyi@gmail.com>
2021-02-04 20:16:48 -0800

    successful field creation

--- mongodb.js
+++ mongodb.js
@@ -8,11 +8,22 @@ const MongoClient = mongodb.MongoClient
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
       return console.log('Unable to connect to database')
     } 
 
-    console.log('connection successful!')
+    const db = client.db(databaseName)
+
+    db.collection('users').insertOne({
+        name: 'Paul Wall',
+        age: 27
+    }, (error, result) => {
+        if (error) {
+            return console.log('Unable to insert user')
+        }
+
+        console.log(result.ops)
+    })
 
 })
