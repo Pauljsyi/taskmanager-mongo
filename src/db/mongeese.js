58753325f1c6de488431cd2a2554feba7ceda39c
Paul Yi <pauljsyi@gmail.com>
2021-02-06 22:51:11 -0800

    using mongoose library to add a new document into collection

--- src/db/mongeese.js
+++ src/db/mongeese.js
@@ -5,26 +5,47 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 })
 
-const User = mongoose.model('User', {
-    name: {
+// const User = mongoose.model('User', {
+//     name: {
+//         type: String
+//     },
+//     age: {
+//         type: Number
+//     },
+//     status: {
+//         type: String
+//     }
+// })
+
+// const me = new User({
+//     name: 'Paul',
+//     age: 30,
+//     status: 'single'
+// })
+
+// me.save().then((me) => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('Error', error)
+// })
+
+
+const Task = mongoose.model('Task', {
+    description: {
         type: String
     },
-    age: {
-        type: Number
-    },
-    status: {
-        type: String
+    completed: {
+        type: Boolean
     }
 })
 
-const me = new User({
-    name: 'Paul',
-    age: 30,
-    status: 'single'
+const task = new Task({
+    description: 'learning how to use Mongoose',
+    completed: false
 })
 
-me.save().then((me) => {
-    console.log(me)
-}).catch((error) => {
-    console.log('Error', error)
+task.save().then(() => {
+    console.log(task)
+}).catch(() => {
+    console.log(error)
 })
