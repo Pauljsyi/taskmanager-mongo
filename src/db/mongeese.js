7af4a1026ac4175b711036004280f671688570eb
Paul Yi <pauljsyi@gmail.com>
2021-02-07 00:00:40 -0800

    custom validator

--- src/db/mongeese.js
+++ src/db/mongeese.js
@@ -5,47 +5,52 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 })
 
-// const User = mongoose.model('User', {
-//     name: {
-//         type: String
-//     },
-//     age: {
-//         type: Number
-//     },
-//     status: {
-//         type: String
-//     }
-// })
-
-// const me = new User({
-//     name: 'Paul',
-//     age: 30,
-//     status: 'single'
-// })
-
-// me.save().then((me) => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error', error)
-// })
-
-
-const Task = mongoose.model('Task', {
-    description: {
+const User = mongoose.model('User', {
+    name: {
         type: String
     },
-    completed: {
-        type: Boolean
+    age: {
+        type: Number,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    },
+    status: {
+        type: String
     }
 })
 
-const task = new Task({
-    description: 'learning how to use Mongoose',
-    completed: false
+const me = new User({
+    name: 'Jessie',
+    age: -1,
+    status: 'single'
 })
 
-task.save().then(() => {
-    console.log(task)
-}).catch(() => {
-    console.log(error)
+me.save().then((me) => {
+    console.log(me)
+}).catch((error) => {
+    console.log('Error', error)
 })
+
+
+// const Task = mongoose.model('Task', {
+//     description: {
+//         type: String
+//     },
+//     completed: {
+//         type: Boolean
+//     }
+// })
+
+// const task = new Task({
+//     description: 'learning how to use Mongoose',
+//     completed: false
+// })
+
+// task.save().then(() => {
+//     console.log(task)
+// }).catch(() => {
+//     console.log(error)
+// })
