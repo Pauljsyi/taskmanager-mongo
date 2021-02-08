ce1feeb397d58ed1e584185faea5d9980070c826
Paul Yi <pauljsyi@gmail.com>
2021-02-07 22:28:12 -0800

    email validator via validator library

--- src/db/mongeese.js
+++ src/db/mongeese.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -9,7 +10,16 @@ const User = mongoose.model('User', {
     name: {
         type: String
     },
-    age: {
+    email: {
+        type: String,
+        required: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+     age: {
         type: Number,
         validate(value) {
             if (value < 0) {
@@ -24,7 +34,7 @@ const User = mongoose.model('User', {
 
 const me = new User({
     name: 'Jessie',
-    age: -1,
+    email: '',
     status: 'single'
 })
 
