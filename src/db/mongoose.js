2895426e11f93717a773dba95eb81b2a6bf28023
Paul Yi <pauljsyi@gmail.com>
2021-02-10 23:06:50 -0800

    validation and sanitization challenge --create new task and default value for absent values

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -7,65 +7,86 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager', {
 })
 
 
-const User = mongoose.model('users', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    }, 
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number')
-            }
-        }
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    password: {
-        type: String,
-        trim: true,
-        validate(value) {
-            // console.log('VALUE',value.length)
-            if (value.length <= 6) {
-                throw new Error('PASSWORD MUST BE LONGER THAN 6 CHARACTERS')
-            } else if (value === "password") {
-                throw new Error('Password cannot be "password"')
-            } else if (value === "PASSWORD") {
-                throw new Error('Password cannot be "PASSWORD"')
-            }
+// const User = mongoose.model('users', {
+//     name: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     }, 
+//     age: {
+//         type: Number,
+//         default: 0,
+//         validate(value) {
+//             if (value < 0) {
+//                 throw new Error('Age must be a positive number')
+//             }
+//         }
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         lowercase: true,
+//         validate(value) {
+//             if (!validator.isEmail(value)) {
+//                 throw new Error('Email is invalid')
+//             }
+//         }
+//     },
+//     password: {
+//         type: String,
+//         trim: true,
+//         validate(value) {
+//             // console.log('VALUE',value.length)
+//             if (value.length <= 6) {
+//                 throw new Error('PASSWORD MUST BE LONGER THAN 6 CHARACTERS')
+//             } else if (value === "password") {
+//                 throw new Error('Password cannot be "password"')
+//             } else if (value === "PASSWORD") {
+//                 throw new Error('Password cannot be "PASSWORD"')
+//             }
 
             
-        }
+//         }
+//     }
+// })
+
+// const me = new User({
+//     name: '   PaUl   ',
+//     email: 'paul@gmail.com',
+//     password: '   PaSsWoRd   '
+// })
+
+// me.save().then((me) => {
+//     console.log(me)
+// }).catch((error) => {
+//     if (error.errors.email.value === ""){
+//        return console.log('email cannot be empty')
+//     } 
+//     // console.log(' catch Error', error)
+//     console.log('Exact Error:', error.errors.email.reason)
+// })
+
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+    },
+    completed: {
+        type: Boolean,
+        required: false,
+        default: false
     }
 })
 
-const me = new User({
-    name: '   PaUl   ',
-    email: 'paul@gmail.com',
-    password: '   PaSsWoRd   '
+
+const task = new Task({
+    description: "order take-out"
 })
 
-me.save().then((me) => {
-    console.log(me)
-}).catch((error) => {
-    if (error.errors.email.value === ""){
-       return console.log('email cannot be empty')
-    } 
-    // console.log(' catch Error', error)
-    console.log('Exact Error:', error.errors.email.reason)
+task.save().then(() => {
+    console.log(task)
+}).catch(() => {
+    console.log(error)
 })
-
-
-
