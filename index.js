8c6c2fd58f291441883e24e2d1760cffd23e637a
Paul Yi <pauljsyi@gmail.com>
2021-02-13 17:39:07 -0800

    reading documents

--- index.js
+++ index.js
@@ -19,6 +19,21 @@ app.post("/users", (req, res) => {
     })
 })
 
+app.get("/users", (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get("/users/:id", (req, res) => {
+    const _id = req.params.id
+    
+
+    console.log(req.params)
+})
+
 app.post("/tasks", (req, res) => {
     const task = new Task(req.body)
 
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,25 +1,6 @@
 const mongoose = require('mongoose')
 
-
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
-
-
-
-
-
-
-
-
-
-// const task = new Task({
-//     description: "order take-out"
-// })
-
-// task.save().then(() => {
-//     console.log(task)
-// }).catch(() => {
-//     console.log(error)
-// })
