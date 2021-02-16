cb885b9b6109b12f8823f22d3f039d613d5594a9
Paul Yi <pauljsyi@gmail.com>
2021-02-15 23:43:28 -0800

    promise chaining

--- src/prac/promises.js
+++ src/prac/promises.js
@@ -6,15 +6,25 @@ const add = (a, b) => {
     })
 }
 
-add(1, 2).then((sum) => {
+// add(1, 2).then((sum) => {
 
+//     console.log(sum)
+
+//     add(sum, 5).then((sum2) => {
+//         console.log(sum2)
+//     }).catch((e) => {
+//         console.log(e)
+//     })
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+
+add(1, 1).then((sum) => {
     console.log(sum)
-
-    add(sum, 5).then((sum2) => {
-        console.log(sum2)
-    }).catch((e) => {
-        console.log(e)
-    })
+    return add(sum, 4)
+}).then((sum2) => {
+    console.log(sum2)
 }).catch((e) => {
     console.log(e)
 })
