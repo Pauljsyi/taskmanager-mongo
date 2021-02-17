ac6710fd2c974dec3e180372444a9f9a590fb399
Paul Yi <pauljsyi@gmail.com>
2021-02-16 22:46:02 -0800

    findByIdAndDelete with tasks

---
+++ src/prac/promise-chaining2.js
@@ -0,0 +1,12 @@
+require("./../db/mongoose")
+const Task = require("./../models/task")
+
+
+Task.findByIdAndDelete("602caa5269ca902f74d425a7").then((task) => {
+    console.log(task)
+    return Task.countDocuments({completed: false})
+}).then((result) => {
+    console.log(result)
+}).catch((e) => {
+    console.log(e)
+})
