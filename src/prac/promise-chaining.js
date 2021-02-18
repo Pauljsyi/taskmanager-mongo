d51d76563f8361de4571ced3f7cca1ed3f00de3e
Paul Yi <pauljsyi@gmail.com>
2021-02-18 00:58:14 -0800

    async/await tasks

--- src/prac/promise-chaining.js
+++ src/prac/promise-chaining.js
@@ -3,11 +3,26 @@ const User = require('./../models/user')
 
 
 
-User.findByIdAndUpdate("60278605ca6257d42f1d42ff", {age: 1}).then((user) => {
-    console.log(user)
-    return User.countDocuments({ age: 1})
-}).then((result) => {
-    console.log(result)
+// User.findByIdAndUpdate("60278605ca6257d42f1d42ff", {age: 1}).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 1})
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+
+//async function
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({ age })
+    return count
+}
+
+updateAgeAndCount("60278605ca6257d42f1d42ff", 2).then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
+
--- src/prac/promise-chaining2.js
+++ src/prac/promise-chaining2.js
@@ -2,14 +2,27 @@ require("./../db/mongoose")
 const Task = require("./../models/task")
 
 
-Task.findByIdAndDelete("602caa5269ca902f74d425a7").then((task) => {
-    console.log(task)
-    return Task.countDocuments({completed: false})
-}).then((result) => {
-    console.log(result)
+// Task.findByIdAndDelete("602caa5269ca902f74d425a7").then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({completed: false})
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+
+//async function
+const deleteTaskAndCount = async (id, completed) => {
+    const task = await Task.findByIdAndDelete(id, {completed})
+    const count = await Task.countDocuments({completed})
+    return count
+}
+
+
+deleteTaskAndCount("602caa5269ca902f74d425a7", false).then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
-
-
 
