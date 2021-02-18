eb14635763fcc1050205a954a0a1fc09d2b3ebfd
Paul Yi <pauljsyi@gmail.com>
2021-02-18 07:44:32 -0800

    refactored tasks read op to asynchronous

--- index.js
+++ index.js
@@ -104,18 +104,28 @@ app.get("/tasks", async (req, res) => {
     // })
 })
 
-app.get("/tasks/:id", (req, res) => {
+app.get("/tasks/:id", async (req, res) => {
     const _id = req.params.id
 
-
-    Task.findById(_id).then((task) => {
+    try {
+        const task = await Task.findById(_id)
         if (!task) {
             return res.status(404).send()
         }
+
         res.send(task)
-    }).catch((e) => {
+    } catch (e) {
         res.status(500).send()
-    })
+    }
+
+    // Task.findById(_id).then((task) => {
+    //     if (!task) {
+    //         return res.status(404).send()
+    //     }
+    //     res.send(task)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
 })
 
 
