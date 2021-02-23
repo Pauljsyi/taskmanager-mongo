b0603fefce5dce0ef966a95a3885c7bc2410638b
Paul Yi <pauljsyi@gmail.com>
2021-02-22 19:33:02 -0800

    removed old ref codes

--- index.js
+++ index.js
@@ -99,13 +99,6 @@ app.get("/tasks", async (req, res) => {
     } catch (e) {
         res.status(401).send()
     }
-
-
-    // Task.find({}).then((tasks) => {
-    //     res.send(tasks)
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
 })
 
 app.get("/tasks/:id", async (req, res) => {
@@ -121,15 +114,6 @@ app.get("/tasks/:id", async (req, res) => {
     } catch (e) {
         res.status(500).send()
     }
-
-    // Task.findById(_id).then((task) => {
-    //     if (!task) {
-    //         return res.status(404).send()
-    //     }
-    //     res.send(task)
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
 })
 
 app.patch('/tasks/:id', async (req, res) => {
