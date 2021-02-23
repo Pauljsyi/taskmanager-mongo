568994632b7aa081b438b892dcbf62726ec0eb2d
Paul Yi <pauljsyi@gmail.com>
2021-02-22 19:28:45 -0800

    delete prac

--- index.js
+++ index.js
@@ -95,11 +95,9 @@ app.delete('/users/:id', async (req, res) => {
 
     try {
         const user = await User.findByIdAndDelete(req.params.id)
-
         if (!user) {
             return res.status(404).send()
         }
-
         res.send(user)
     } catch(e) {
         res.status(500).send()
